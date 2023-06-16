b65511368b668ee5e20a1f30410dc605c1dd116a
vmartuniyk <martuniyk93@ukr.net>
2023-06-16 12:22:08 +0300

    Hight order test register page

--- tests/Feature/RegisterTest.php
+++ tests/Feature/RegisterTest.php
@@ -3,11 +3,14 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 uses(RefreshDatabase::class);
 
-it('has errors if the details are not provided', function () {
-    $this->post('/register')
-         ->assertSessionHasErrors(['name','email','password']);
+it('it shows register page')
+    ->get('/auth/register')
+    ->assertStatus(200);
 
-});
+it('has errors if the details are not provided')
+        ->post('/register')
+        ->assertSessionHasErrors(['name','email','password']);
+
 it('register user', function () {
     $this->post('/register',[
         'name' => 'John',
